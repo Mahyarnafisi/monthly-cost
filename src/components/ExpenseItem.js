b936c3400cf8bb4d16eb3e9b00f26129690bd02c
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-08 15:23:06 +0200

    finish styling ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,12 @@
 import React from "react";
-
+import "../style/ExpenseItem.css";
 function ExpenseItem() {
-  return <div>expenseitem</div>;
+  return (
+    <div className="expense-item">
+      <div className="expense-item__date">27th April 2020</div>
+      <div className="expense-item__description">Car insurance</div>
+      <div className="expense-item__price">$239</div>
+    </div>
+  );
 }
 export default ExpenseItem;
