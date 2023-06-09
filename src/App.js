a0d0cb5a1d218af90d2d24e6b2eb1e01425f0ef8
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-09 11:37:47 +0200

    fix style in index.csss

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   // Dummy data list
   const expense = [
     { id: 1, title: "car insurance", date: "24th April 2021", amount: 239 },
-    { id: 1, title: "Books", date: "27th April 2021", amount: 199 },
-    { id: 1, title: "Saving", date: "30th March 2021", amount: 829 },
+    { id: 1, title: "Books", date: "27th September 2021", amount: 199 },
+    { id: 1, title: "Saving", date: "30th November 2021", amount: 829 },
     { id: 1, title: "Groceries", date: "29th March 2021", amount: 389 },
   ];
   //
