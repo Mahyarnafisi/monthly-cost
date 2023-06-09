38596dec1ec4aed2efdfbba657c73d7403bcbd3e
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-09 11:18:21 +0200

    fix size Expenses components

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -6,7 +6,6 @@ function Expenses(props) {
     <div className="expenses">
       <ExpenseItem title={props.item[0].title} date={props.item[0].date} amount={props.item[0].amount} /> <ExpenseItem title={props.item[1].title} date={props.item[1].date} amount={props.item[1].amount} />
       <ExpenseItem title={props.item[2].title} date={props.item[2].date} amount={props.item[2].amount} /> <ExpenseItem title={props.item[3].title} date={props.item[3].date} amount={props.item[3].amount} />
-      <ExpenseItem title={props.item[2].title} date={props.item[2].date} amount={props.item[2].amount} /> <ExpenseItem title={props.item[3].title} date={props.item[3].date} amount={props.item[3].amount} />
     </div>
   );
 }
