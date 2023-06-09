8b5a28a4769a29f6c67860e6781a62dd7c636d88
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-09 19:34:09 +0200

    add ne clickhandler and test evenet on ExpenseItem components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import "../style/ExpenseItem.css";
 import ExpenseItemDate from "./ExpenseItemDate";
 
 function ExpenseItem(props) {
+  let update = props.title;
+  const clickHandler = () => {
+    update = "updated";
+  };
   return (
     <div className="expense-item">
       <ExpenseItemDate date={props.date} />
-      <div className="expense-item__description">{props.title}</div>
+      <div className="expense-item__description">{update}</div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>click here</button>
     </div>
   );
 }
