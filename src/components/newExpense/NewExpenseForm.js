b2f73b355449a401f49d87736954a85ea3f62636
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-10 23:23:24 +0200

    new style in newexpenseform component

--- src/components/newExpense/NewExpenseForm.js
+++ src/components/newExpense/NewExpenseForm.js
@@ -1,25 +1,33 @@
 import React from "react";
 import "../../style/newexpense/NewExpenseForm.css";
 import { BsPlusLg } from "react-icons/bs";
+import { BsChevronRight } from "react-icons/bs";
 
 function NewExpenseForm() {
   return (
     <form className="newexpenseform" method="POST">
       <div className="input--title hover-bright">
-        <label htmlFor="inputTitle">Title</label>
+        <label htmlFor="inputTitle">
+          Title <BsChevronRight className="label-icon" />
+        </label>
         <input type="text" name="inputTitle" required placeholder="Title" />
       </div>
 
       <div className="input--amount hover-bright">
-        <label htmlFor="inputAmount">Amount</label>
-        <input type="number" name="inputAmount" required placeholder="Amount" min={0.01} step={0.1} />
+        <label htmlFor="inputAmount">
+          Amount <BsChevronRight className="label-icon" />
+        </label>
+        <input type="number" name="inputAmount" required placeholder="Amount" />
       </div>
 
       <div className="input--date hover-bright">
+        <label htmlFor="inputDate">
+          Date <BsChevronRight className="label-icon" />
+        </label>
         <input type="date" name="inputDate" required min="2019-01-01" max="2020-01-01" />
       </div>
 
-      <button className="expenseform__submit hover-darker">
+      <button className="expenseform__submit hover-bright">
         <BsPlusLg />
       </button>
     </form>
