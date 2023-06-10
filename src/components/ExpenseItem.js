d177ed741c36670a3a2d9dc2242b6c6576b6bc0e
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-10 18:09:04 +0200

    add useState hook to expenseItem and testing abort click handler function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,14 +4,16 @@ import "../style/ExpenseItem.css";
 import ExpenseItemDate from "./ExpenseItemDate";
 
 function ExpenseItem(props) {
-  let update = props.title;
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
-    update = "updated";
+    setTitle(() => {
+      return "updated";
+    });
   };
   return (
     <div className="expense-item">
       <ExpenseItemDate date={props.date} />
-      <div className="expense-item__description">{update}</div>
+      <div className="expense-item__description">{title}</div>
       <div className="expense-item__price">${props.amount}</div>
       <button onClick={clickHandler}>click here</button>
     </div>
