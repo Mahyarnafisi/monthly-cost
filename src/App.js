fbf66f8b2e687acde094addfdbfd0eb20c7acfb8
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-12 22:06:59 +0200

    initiate dynamic state full array on expense list

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import Expenses from "./components/expenses/Expenses";
 import NewExpense from "./components/newExpense/NewExpense";
 import ExpenseFilter from "./components/expenses/ExpenseFilter";
 
+// Dummy data list
+const initiateExpense = [
+  { id: 1, title: "car insurance", date: "24th April 2021", amount: 239 },
+  { id: 2, title: "Books", date: "27th September 2021", amount: 199 },
+  { id: 3, title: "Saving", date: "30th November 2021", amount: 829 },
+  { id: 4, title: "Groceries", date: "29th March 2021", amount: 389 },
+  { id: 5, title: "Jeans and shoes", date: "29th November 2019", amount: 299 },
+];
 function App() {
+  const [expenses, setExpenses] = useState(initiateExpense);
   // State section
   const [filterYear, setFilterYear] = useState("2022");
-  // Dummy data list
-  const expense = [
-    { id: 1, title: "car insurance", date: "24th April 2021", amount: 239 },
-    { id: 2, title: "Books", date: "27th September 2021", amount: 199 },
-    { id: 3, title: "Saving", date: "30th November 2021", amount: 829 },
-    { id: 4, title: "Groceries", date: "29th March 2021", amount: 389 },
-    { id: 5, title: "Jeans and shoes", date: "29th November 2019", amount: 299 },
-  ];
+
   //climb up data from form expenses
   const addExpensesHandler = (data) => {
-    const expenses = {
-      ...data,
-    };
-    console.log("from app js");
-    console.log(expenses);
+    setExpenses((prevExpenses) => {
+      return [data, ...prevExpenses];
+    });
   };
 
   // climb up data from filter selected and save it in state
@@ -31,7 +31,7 @@ function App() {
     <div className="App">
       <ExpenseFilter defaultValue={filterYear} onFilterChange={selectedYearFilter} />
       <NewExpense onAddExpenses={addExpensesHandler} />
-      <Expenses item={expense} />
+      <Expenses item={expenses} />
     </div>
   );
 }
--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -5,9 +5,9 @@ import "../../style/expenses/Expenses.css";
 function Expenses(props) {
   return (
     <div className="expenses">
-      <ExpenseItem title={props.item[0].title} date={props.item[0].date} amount={props.item[0].amount} /> <ExpenseItem title={props.item[1].title} date={props.item[1].date} amount={props.item[1].amount} />
-      <ExpenseItem title={props.item[2].title} date={props.item[2].date} amount={props.item[2].amount} /> <ExpenseItem title={props.item[3].title} date={props.item[3].date} amount={props.item[3].amount} />
-      <ExpenseItem title={props.item[4].title} date={props.item[4].date} amount={props.item[4].amount} />
+      {props.item.map((item) => (
+        <ExpenseItem title={item.title} date={item.date} amount={item.amount} />
+      ))}
     </div>
   );
 }
